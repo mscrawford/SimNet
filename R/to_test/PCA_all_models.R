69b3816726c5902709d7c1e1072f44406f201026
veronika_cn <veronikaceballosnunez@gmail.com>
2022-03-04 10:13:58 +0100

    Cleaned up the code for PCA

--- R/to_test/PCA_all_models.R
+++ R/to_test/PCA_all_models.R
@@ -1,7 +1,3 @@
-library("pls")
-library("factoextra")
-library(FactoMineR)
-library(corrplot)
 #setwd(dirname(rstudioapi::getActiveDocumentContext()$path)); setwd("../../")
 #base_dir          <- getwd()
 base_dir          <- setwd("../../")
@@ -11,88 +7,36 @@ raw_data_dir      <- paste0(base_dir, "/data/raw")
 
 source(paste0(scripts_dir, "/to_test/fx_PCA_all_models.R"))
 
-### Grass1 (Adam's model)
-#source(paste0(scripts_dir, "/to_test/readAdam.R"))
-#
-#adam <- models$Grass1 %>%
-#  ungroup() %>% # There shouldn't be groups anyways
-#  mutate(Biomass = scales::rescale(Biomass,
-#                                   to = c(0, 100)))%>%
-#  #select(-Model, -SeedRain, -abmi) #Removed abmi because it is highly correlated to biomass
-#  select(-Model, -SeedRain)
-#
-#G1C1 <- fx_PCA_all_models("Grass1",adam)
-#
-#### Grass2 (Lindsay's model)
-#source(paste0(scripts_dir, "/to_test/readLindsay.R"))
-#
-#lindsay <- models$Grass2%>%
-#    ungroup() %>% # There shouldn't be groups anyways
-#    mutate(Biomass = scales::rescale(Biomass,
-#                                     to = c(0, 100)))%>%
-#    select(-Model, -SeedRain) 
-#
-#G2C1 <- fx_PCA_all_models("Grass2",lindsay)
-#
 ### Grass3 (IBC-grass)
 source(paste0(scripts_dir, "/to_test/readIBC.R"))
 
-#IBC_grass <- models$Grass3 %>%
-#  ungroup() %>% # There shouldn't be groups anyways
-#  mutate(Biomass = scales::rescale(Biomass,
-#                                   to = c(0, 100)))%>%
-#  select(-Model, -SeedRain) %>%
-#  select(Rep, Ninitial, SpeciesID, Year, Stage, Productivity, Biomass, LMR, MaxMass, Gmax, SLA, meanSpacerLength)
-# 
-#
-#G3C1 <- fx_PCA_all_models("Grass3",IBC_grass)
-##pcr_model <- pcr(Biomass~LMR+MaxMass+Gmax+SLA+meanSpacerLength, data=IBC_grass, scale =TRUE, validation="CV")
-##summary(pcr_model)
-#fviz_eig(G3C1, addlabels = TRUE, ylim = c(0, 50))
-#ggsave(paste0(tmp_dir,"/PCA/ScreePlot_Grass3.pdf")) #Eigenvals organized from largest to smallest
-#while (!is.null(dev.list()))  dev.off()
-#
-#var <- get_pca_var(G3C1)
-#corrplot(var$contrib, is.corr=FALSE)
-#recordPlot()
-##ggsave(paste0(tmp_dir,"/PCA/Corr_Grass3.pdf")) #Highlight most contributing variables to each dimension
-##while (!is.null(dev.list()))  dev.off()
-#
-##res.desc <- dimdesc(G3C1, axes = c(1,2), proba = 0.05)
-### Description of dimension 1
-##print(res.desc$Dim.1)
-#
-#ind <- get_pca_ind(G3C1)
-#df <- ind$coord
-#
-#IBC_grass$PC1score <- df[,1]
-#IBC_grass$PC2score <- df[,2]
-#IBC_grass$PC3score <- df[,3]
-#fileName = paste0(tmp_dir,"/PCA/Grass3_PCAcoord.Rda")
-#saveRDS(IBC_grass,file=fileName)
+IBC_grass <- models$Grass3 %>%
+  ungroup() %>% # There shouldn't be groups anyways
+  mutate(Biomass = scales::rescale(Biomass,
+                                   to = c(0, 100)))%>%
+  select(-Model, -SeedRain) %>%
+  select(Rep, Ninitial, SpeciesID, Year, Stage, Productivity, Biomass, LMR, MaxMass, Gmax, SLA, meanSpacerLength)
+ 
+G3PCA <- fx_PCA("Grass3",IBC_grass)
+fviz_eig(G3PCA, addlabels = TRUE, ylim = c(0, 50))
+ggsave(paste0(tmp_dir,"/PCA/ScreePlot_Grass3.pdf")) #Eigenvals organized from largest to smallest
+while (!is.null(dev.list()))  dev.off()
 
-#### Forest1 (PPA)
-#source(paste0(scripts_dir, "/to_test/readPPA.R"))
-#
-#PPA <- models$Forest1 %>%
-#  ungroup() %>% # There shouldn't be groups anyways
-#  mutate(Biomass = scales::rescale(Biomass,
-#                                   to = c(0, 100)))%>%
-#  select(-Model, -SeedRain)
-#
-#F1C1 <- fx_PCA_all_models("Forest1",PPA)
-#
-#### Forest2 (TROLL)
-#source(paste0(scripts_dir, "/to_test/readTROLL.R"))
-#
-#troll <- models$Forest2 %>%
-#    ungroup() %>% # There shouldn't be groups anyways
-#    mutate(Biomass = scales::rescale(Biomass,
-#                                     to = c(0, 100)))%>%
-#    select(-Model, -SeedRain)
-#
-#F2C1 <- fx_PCA_all_models("Forest2",troll)
-#
+modelName <- "Grass3"
+G3PCA_corr <- fx_correlation_plot(modelName,G3PCA)
+
+ind <- get_pca_ind(G3PCA)
+df <- ind$coord
+
+IBC_grass$PC1score <- df[,1]
+IBC_grass$PC2score <- df[,2]
+IBC_grass$PC3score <- df[,3]
+fileName = paste0(tmp_dir,"/PCA/Grass3_PCAcoord.Rda")
+saveRDS(IBC_grass,file=fileName)
+
+#pcr_model <- pcr(Biomass~LMR+MaxMass+Gmax+SLA+meanSpacerLength, data=IBC_grass, scale =TRUE, validation="CV")
+#summary(pcr_model)
+
 ### Forest2 (TROLL)
 source(paste0(scripts_dir, "/to_test/readTROLL.R"))
 
@@ -100,21 +44,16 @@ troll <- models$Forest2 %>%
     ungroup() %>% # There shouldn't be groups anyways
     mutate(Biomass = scales::rescale(Biomass,
                                      to = c(0, 100)))%>%
-    select(-Model, -SeedRain) %>%
-    mutate(h_realmax = hmax * dmax / (dmax + ah)) %>%
-    select(-hmax, -ah, -dmax)
-
-F2PCA <- fx_PCA_all_models("Forest2_hrm",troll)
-#pcr_model <- pcr(Biomass~h_realmax+wsg+lma+pmass+nmass, data=troll, scale =TRUE, validation="CV")
-#summary(pcr_model)
+    select(-Model, -SeedRain)
+modelName <- "Forest2"
+F2PCA <- fx_PCA(modelName,troll)
 fviz_eig(F2PCA, addlabels = TRUE, ylim = c(0, 50))
 ggsave(paste0(tmp_dir,"/PCA/ScreePlot_Forest2.pdf")) #Eigenvals organized from largest to smallest
 while (!is.null(dev.list()))  dev.off()
-var <- get_pca_var(F2PCA)
-corrplot(var$contrib, is.corr=FALSE)
-recordPlot()
 
-F2PCR <- pcr(Productivity~h_realmax+wsg+lma+pmass+nmass, data=troll, scale =TRUE, validation="CV")
+F2PCA_corr <- fx_correlation_plot(modelName,F2PCA)
+
+F2PCR <- pcr(Productivity~hmax+dmax+ah+wsg+lma+pmass+nmass, data=troll, scale =TRUE, validation="CV")
 #summary(pcr_model)
 ind <- get_pca_ind(F2PCA)
 df <- ind$coord
@@ -125,13 +64,38 @@ troll$PC3score <- df[,3]
 fileName = paste0(tmp_dir,"/PCA/Forest2_PCAcoord.Rda")
 saveRDS(troll,file=fileName)
 
-#### Dryland (Bjoern)
-#source(paste0(scripts_dir, "/to_test/readBjoern.R"))
-#
-#bjoern <- models$bjoern %>% 
-#    ungroup() %>% # There shouldn't be groups anyways
-#    mutate(Biomass = scales::rescale(Biomass,
-#                                     to = c(0, 100)))%>%
-#    select(-Model, -SeedRain, -pRoot) 
-#
-#DC1 <- fx_PCA_all_models("Dryland",bjoern)
+#pcr_model <- pcr(Biomass~h_realmax+wsg+lma+pmass+nmass, data=troll, scale =TRUE, validation="CV")
+#summary(pcr_model)
+
+### Forest2 (TROLL)
+source(paste0(scripts_dir, "/to_test/readTROLL.R"))
+
+troll <- models$Forest2 %>%
+   ungroup() %>% # There shouldn't be groups anyways
+   mutate(Biomass = scales::rescale(Biomass,
+                                    to = c(0, 100)))%>%
+   select(-Model, -SeedRain) %>%
+   mutate(h_realmax = hmax * dmax / (dmax + ah)) %>%
+   select(-hmax, -ah, -dmax)
+
+F2PCA_hrm <- fx_PCA("Forest2_hrm",troll)
+fviz_eig(F2PCA, addlabels = TRUE, ylim = c(0, 50))
+ggsave(paste0(tmp_dir,"/PCA/ScreePlot_Forest2_hrm.pdf")) #Eigenvals organized from largest to smallest
+while (!is.null(dev.list()))  dev.off()
+
+modelName <- "Forest2_hrm"
+F2PCA_hrm_corr <- fx_correlation_plot(modelName,F2PCA_hrm)
+
+F2PCR <- pcr(Productivity~h_realmax+wsg+lma+pmass+nmass, data=troll, scale =TRUE, validation="CV")
+#summary(pcr_model)
+ind <- get_pca_ind(F2PCA)
+df <- ind$coord
+
+troll$PC1score <- df[,1]
+troll$PC2score <- df[,2]
+troll$PC3score <- df[,3]
+fileName = paste0(tmp_dir,"/PCA/Forest2_hrm_PCAcoord.Rda")
+saveRDS(troll,file=fileName)
+
+#pcr_model <- pcr(Biomass~h_realmax+wsg+lma+pmass+nmass, data=troll, scale =TRUE, validation="CV")
+#summary(pcr_model)
--- R/to_test/fx_PCA_all_models.R
+++ R/to_test/fx_PCA_all_models.R
@@ -1,12 +1,15 @@
 library(ggbiplot)
-library("party")
 library("pls")
-set.seed(42)
+library("factoextra")
+library(FactoMineR)
+library(corrplot)
+set.seed(1987)
 
 meta = seq(80, 100)
 iso = seq(180, 200)
 
-fx_PCA_all_models <- function(modelName,model){
+fx_PCA <- function(modelName,model){
+# Perform a Principal Components Analysis for a given model and produce a biplot
 	print(paste0("PCA: ",modelName))
         model <- model %>%
             mutate_if(is.character, as.factor) %>%
@@ -18,20 +21,36 @@ fx_PCA_all_models <- function(modelName,model){
         #print(model.pca$center)
         #print(model.pca$rotation)
         
-#	ggbiplot(mtcars.pca,ellipse=TRUE,obs.scale = 1, var.scale = 1,  labels=rownames(mtcars), groups=mtcars.country) +
+	#ggbiplot(mtcars.pca,ellipse=TRUE,obs.scale = 1, var.scale = 1,  labels=rownames(mtcars), groups=mtcars.country) +
         ggbiplot(model.pca, scale=0) +
 		scale_colour_manual(name="Origin", values= c("forest green", "red3", "dark blue")) +
 		ggtitle(paste0("PCA of ", modelName)) +
 	       	theme_minimal() +
-	       	theme(legend.position = "bottom")
+	       	theme(text = element_text(size = 16))
 
         ggsave(paste0(tmp_dir,"/PCA/PCA_",modelName,".pdf"))
+        #ggsave(paste0(tmp_dir,"/PCA/PCA_",modelName,".pdf"), width=9, height=7, dpi=300)
         while (!is.null(dev.list()))  dev.off()
         return(model.pca)
 }
 
-##Principal components regression
-#fx_PCR <- function(modelName,model){
-#	print(paste0("PCR: ",modelName))
+fx_get_coordinates <- function(modelID,PCAresult,fileName){
+	ind <- get_pca_ind(PCAresult)
+	df <- ind$coord
+	modelID$PC1score <- df[,1]
+	modelID$PC2score <- df[,2]
+	modelID$PC3score <- df[,3]
+	saveRDS(modelID,file=fileName)
+	return()
+}
 
-
+fx_correlation_plot <- function(modelName,PCAresult){
+#Correlation plot to highlight most contributing variables to each dimension
+	var <- get_pca_var(PCAresult)
+	pdf(file = paste0(tmp_dir,"/PCA/Corr_",modelName,".pdf"), family = "sans")
+	p <- corrplot(var$contrib, is.corr=FALSE,col=colorRampPalette(c("#B2182B","#FDDBC7","#2166AC"))(100),
+		 title = "Contribution of variables to each dimension", 
+		 mar = c(0,0,1,0), tl.cex = 1.5, , font.main = 1, cex.axis = 1.5, cex.main = 1.7, number.digits = 1)
+	dev.off()
+	return(p)
+}
