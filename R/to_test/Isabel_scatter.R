e0b264edcd863ae375a2ddd8e4bede73aeb10314
Veronika <ar07wiga@idivnb244.usr.idiv.de>
2024-04-15 15:38:08 +0200

    Cleaning up code for Figure 3, and changing order of plots (according to reviewers

--- R/to_test/Isabel_scatter.R
+++ R/to_test/Isabel_scatter.R
@@ -1,5 +1,10 @@
+library(ggplot2)
+library(ggh4x)
 library(png)
 library(readr)
+library(data.table)
+library(tidyr)
+library(dplyr)
 
 base_dir          <- setwd("../../")
 scripts_dir       <- paste0(base_dir, "/R")
@@ -8,29 +13,47 @@ raw_data_dir      <- paste0(base_dir, "/data/raw")
 store_dir         <- paste0(tmp_dir,"/traits_vs_biomass/")
 
 #source(paste0(scripts_dir, "/readModels.R"))
-source(paste0(scripts_dir, "/to_test/fx_traits_vs_biomass.R"))
+#source(paste0(scripts_dir, "/to_test/fx_traits_vs_biomass.R"))
 source(paste0(scripts_dir, "/to_test/fx_cforest_party.R"))
 
-#READ_CACHE <- TRUE 
-#SAVE_CACHE <- FALSE
-READ_CACHE <- FALSE
-SAVE_CACHE <- TRUE 
+READ_CACHE <- TRUE 
+SAVE_CACHE <- FALSE
+#READ_CACHE <- FALSE
+#SAVE_CACHE <- TRUE 
 
-#### Grass1 (Adam's model)
-#file_name <- paste0(store_dir,"adam.csv")
-#if(SAVE_CACHE){adam <- fx_read_model("readAdam.R", "Grass1") %>%
-#	       mutate(r_pNi = 1/pNi) #rpNi = reciprocal of pNi
-#               write.csv(adam, file_name, row.names=FALSE)
-#}else{adam <- read_csv(file_name)}
-#
-#lab1 <- "monoBiomass" #"no3i (nitrogen R*)"
-#lab2 <- "NUE1" #"r_pNi (reciprocal of \n aboveground N concentration)"
-#
-#d1 <- fx_prepare_df("Grass 1",adam,"abmi","r_pNi",lab1,lab2) 
-#fx_scatter_plot("mono",d1,lab1,"G1C1") 
-#fx_scatter_plot("mix",d1,lab1,"G1C2") 
-#fx_scatter_plot("mono",d1,lab2,"G1C3") 
-#fx_scatter_plot("mix",d1,lab2,"G1C4") 
+fx_prepare_df  <- function(modelName,data,trait1,trait2){
+	dfmono <- data %>%
+	filter(Ninitial == 1, Year %in% meta) %>%
+	select(SpeciesID, Biomass, trait1, trait2)
+    dfmono$type_bm <- rep("Monoculture",times=dim(dfmono)[1])
+
+	dfmix <- data %>%
+	filter(Ninitial == 32, Year %in% meta) %>%
+	select(SpeciesID, Biomass, trait1, trait2) #%>%
+	dfmix <- aggregate(. ~ SpeciesID, data = dfmix, mean) #Calculate mean response per specie
+	dfmix$type_bm <- rep("Mixture",times=dim(dfmix)[1])
+
+	df <- rbind(dfmono,dfmix) %>%
+	setnames(old=c(trait1,trait2), new=c("trait1","trait2"))
+
+	df$Model <- rep(modelName,times=dim(df)[1]) 
+    print(head(df))
+    return(df)
+}
+
+### Grass1 (Adam's model)
+file_name <- paste0(store_dir,"adam.csv")
+if(SAVE_CACHE){adam <- fx_read_model("readAdam.R", "Grass1") %>%
+	       mutate(r_pNi = 1/pNi) #rpNi = reciprocal of pNi
+               write.csv(adam, file_name, row.names=FALSE)
+}else{adam <- read_csv(file_name)}
+
+lab1 <- "monoBiomass" #"no3i (nitrogen R*)"
+lab2 <- "NUE1" #"r_pNi (reciprocal of \n aboveground N concentration)"
+
+d1 <- fx_prepare_df("Grass 1",adam,"abmi","r_pNi") 
+D1 <- data.frame(var=c("trait1","trait2"), Lab = c(lab1,lab2))
+D1$Model <- rep("Grass 1",times=2)
 
 ### Grass2 (Lindsay's model)
 
@@ -44,12 +67,10 @@ if(SAVE_CACHE){lindsay <- fx_read_model("readLindsay_variable.R","Grass2") %>%
 lab1 <- "rootingVolume" #"Vi (volume of soil \n accessible to species i)"
 lab2 <- "NUE2" #thetai (Nitrogen uptake \n rate per unit plant biomass)"
 
-d2 <- fx_prepare_df("Grass 2",lindsay,"Vi","thetai",lab1,lab2)
-fx_scatter_plot("mono",d2,lab1,"G2C1_noloess") 
-fx_scatter_plot("mix",d2,lab1,"G2C2_noloess") 
-fx_scatter_plot("mono",d2,lab2,"G2C3_noloess") 
-fx_scatter_plot("mix",d2,lab2,"G2C4_noloess") 
-prin()
+d2 <- fx_prepare_df("Grass 2",lindsay,"Vi","thetai")
+D2 <- data.frame(var=c("trait1","trait2"), Lab = c(lab1,lab2))
+D2$Model <- rep("Grass 2",times=2)
+
 ### Grass3 (IBC-grass)
 
 file_name <- paste0(store_dir,"IBC_grass.csv")
@@ -62,15 +83,13 @@ if(SAVE_CACHE){IBC_grass <- readRDS(paste0(tmp_dir,"/PCA/Grass3_PCAcoord.Rda")) 
                write.csv(IBC_grass, file_name, row.names=FALSE)
 }else{IBC_grass <- read_csv(file_name)}
 
-lab1 <- "LES1" #"PC1score associated with LMR \n (leaf to mass ratio)"
-lab2 <- "Size/Growth" #"PC2score associated with Gmax \n (maximum resource utilization) \n and MaxMass (Plant's maximum size)"
+lab1 <- "Size/Growth" #"PC2score associated with Gmax \n (maximum resource utilization) \n and MaxMass (Plant's maximum size)"
+lab2 <- "LES1" #"PC1score associated with LMR \n (leaf to mass ratio)"
 lab3 <- "Spacing" #"PC3score associated with SLA and MeanSpacerLength"
 
-d3 <- fx_prepare_df("Grass 3",IBC_grass,"PC2score","PC1score",lab2,lab1)
-fx_scatter_plot("mono",d3,lab1,"G3C1") 
-fx_scatter_plot("mix",d3,lab1,"G3C2") 
-fx_scatter_plot("mono",d3,lab2,"G3C3") 
-fx_scatter_plot("mix",d3,lab2,"G3C4") 
+d3 <- fx_prepare_df("Grass 3",IBC_grass,"PC2score","PC1score")
+D3 <- data.frame(var=c("trait1","trait2"), Lab = c(lab1,lab2))
+D3$Model <- rep("Grass 3",times=2)
 
 #### Forest1 (PPA)
 
@@ -79,14 +98,12 @@ if(SAVE_CACHE){PPA <- fx_read_model("readPPA.R","Forest1")
                write.csv(PPA, file_name, row.names=FALSE)
 }else{PPA <- read_csv(file_name)}
 
-lab1 <- "GrowthSurvival" #"paceOfLife" #"PC1score (associated \n with fast-slow lifecycle)"#plant height)"
-lab2 <- "MaxHeight" #"PC2score (associated \n with tree stature)"# LMA -leaf mass per area)"
+lab1 <- "MaxHeight" #"PC2score (associated \n with tree stature)"# LMA -leaf mass per area)"
+lab2 <- "GrowthSurvival" #"paceOfLife" #"PC1score (associated \n with fast-slow lifecycle)"#plant height)"
 
-d4 <- fx_prepare_df("Forest 1",PPA,"PC2score","PC1score",lab2,lab1)
-fx_scatter_plot("mono",d4,lab1,"F1C1") 
-fx_scatter_plot("mix",d4,lab1,"F1C2") 
-fx_scatter_plot("mono",d4,lab2,"F1C3") 
-fx_scatter_plot("mix",d4,lab2,"F1C4") 
+d4 <- fx_prepare_df("Forest 1",PPA,"PC2score","PC1score")
+D4 <- data.frame(var=c("trait1","trait2"), Lab = c(lab1,lab2))
+D4$Model <- rep("Forest 1",times=2)
 
 ### Forest2 (TROLL) h_realmax
 
@@ -99,15 +116,13 @@ if(SAVE_CACHE){troll <- readRDS(paste0(tmp_dir,"/PCA/Forest2_hrm_PCAcoord.Rda"))
 	       mutate_if(is.character, as.factor)
                write.csv(troll, file_name, row.names=FALSE)
 }else{troll <- read_csv(file_name)}
-lab1 <- "LES2" #"PC1score associated with \n LMA, nmass, and pmass"
-lab2 <- "MaxHeight" #"PC2score associated with \n h_realmax = hmax * dmax / (dmax + ah)"
+lab1 <- "MaxHeight" #"PC2score associated with \n h_realmax = hmax * dmax / (dmax + ah)"
+lab2 <- "LES2" #"PC1score associated with \n LMA, nmass, and pmass"
 lab3 <- "woodDensity" #"PC3score associated with \n wsg (wood specific gravity)"
 
-d5 <- fx_prepare_df("Forest 2",troll,"PC2score","PC1score",lab2,lab1)
-fx_scatter_plot("mono",d5,lab1,"F2C1") 
-fx_scatter_plot("mix",d5,lab1,"F2C2") 
-fx_scatter_plot("mono",d5,lab2,"F2C3") 
-fx_scatter_plot("mix",d5,lab2,"F2C4") 
+d5 <- fx_prepare_df("Forest 2",troll,"PC2score","PC1score")
+D5 <- data.frame(var=c("trait1","trait2"), Lab = c(lab1,lab2))
+D5$Model <- rep("Forest 2",times=2)
 
 ### Dryland (Bjoern)
 
@@ -123,114 +138,44 @@ lab2 <- "leafAllocation" #"pLeaf (allocation \n to leaf) [gC/gC]"
 lab3 <- "storageAllocation" #"pStorage (allocation \n to storage) [gC/gC]"
 lab4 <- "rootsAllocation" #"pRoot (allocation \n to root) [gC/gC]"
 
-d6 <- fx_prepare_df("Dryland",bjoern,"maxSize","pLeaf",lab1,lab2)
-fx_scatter_plot("mono",d6,lab1,"DC1") 
-fx_scatter_plot("mix",d6,lab1,"DC2") 
-fx_scatter_plot("mono",d6,lab2,"DC3") 
-fx_scatter_plot("mix",d6,lab2,"DC4") 
+d6 <- fx_prepare_df("Dryland",bjoern,"maxSize","pLeaf")
+D6 <- data.frame(var=c("trait1","trait2"), Lab = c(lab1,lab2))
+D6$Model <- rep("Dryland",times=2)
 
 #########################################################################################
+print("##############################    Scatter    ##########################")
 #########################################################################################
-#########################################################################################
 
-#all_d <- rbind(d1,d2,d3,d4,d5,d6)
-#plot <- fx_plot_all(all_d)
-#plot <- fx_single_plot("Grass 1",adam,"abmi","r_pNi",lab1,lab2) 
-#plot <- fx_biomass_vs_traits(all_d)
+df <- rbind(d1,d2,d3,d4,d5,d6)
+labels <- rbind(D1,D2,D3,D4,D5,D6)
 
-#### Compile figure 3 in the manuscript: group all plots: 
-dir <- paste0(tmp_dir,"/traits_vs_biomass/")
-G1C1 <- readPNG(paste0(dir,"G1C1.png"))
-G1C2 <- readPNG(paste0(dir,"G1C2.png"))
-G1C3 <- readPNG(paste0(dir,"G1C3.png"))
-G1C4 <- readPNG(paste0(dir,"G1C4.png"))
-G2C1 <- readPNG(paste0(dir,"G2C1.png"))
-G2C3 <- readPNG(paste0(dir,"G2C3.png"))
-G3C1 <- readPNG(paste0(dir,"G3C1.png"))
-G3C3 <- readPNG(paste0(dir,"G3C3.png"))
-F1C1 <- readPNG(paste0(dir,"F1C1.png"))
-F1C3 <- readPNG(paste0(dir,"F1C3.png"))
-F2C1 <- readPNG(paste0(dir,"F2C1.png"))
-F2C3 <- readPNG(paste0(dir,"F2C3.png"))
-G2C2 <- readPNG(paste0(dir,"G2C2.png"))
-G2C4 <- readPNG(paste0(dir,"G2C4.png"))
-G3C2 <- readPNG(paste0(dir,"G3C2.png"))
-G3C4 <- readPNG(paste0(dir,"G3C4.png"))
-F1C2 <- readPNG(paste0(dir,"F1C2.png"))
-F1C4 <- readPNG(paste0(dir,"F1C4.png"))
-F2C2 <- readPNG(paste0(dir,"F2C2.png"))
-F2C4 <- readPNG(paste0(dir,"F2C4.png"))
-#F2C1 <- readPNG(paste0(dir,"F2C1_hrm.png"))
-#F2C3 <- readPNG(paste0(dir,"F2C3_hrm.png"))
-DC1 <- readPNG(paste0(dir,"DC1.png"))
-DC3 <- readPNG(paste0(dir,"DC3.png"))
-DC2 <- readPNG(paste0(dir,"DC2.png"))
-DC4 <- readPNG(paste0(dir,"DC4.png"))
-#F1C1_P <- readPNG(paste0(dir,"F1C1_P.png"))
-#F1C3_P <- readPNG(paste0(dir,"F1C3_P.png"))
-#F2C1_P <- readPNG(paste0(dir,"F2C1_hrm_P.png"))
-#F2C3_P <- readPNG(paste0(dir,"F2C3_hrm_P.png"))
-#DC1_P <- readPNG(paste0(dir,"DC1_P.png"))
-#DC3_P <- readPNG(paste0(dir,"DC3_P.png"))
+df$SpeciesID <- as.character(df$SpeciesID)
+df$log_bm <- log(df$Biomass)
 
-#png(paste0(dir, "all_scatter_plots_biomass.png"), 
-png(paste0(dir, "all_scatter_plots.png"), 
-    #width = 1663, height = 3061, units = "px", pointsize = 40,  res = NA,
-    width = 3326, height = 3061, units = "px", pointsize = 40,  res = NA,
-    bg = "white", type = c("cairo", "cairo-png", "Xlib", "quartz"))
-par(mar=c(0,0,2,0))
-#all_scatter <- plot(NA, xlim = c(0, 2), ylim = c(0, 6), type = "n", 
-all_scatter <- plot(NA, xlim = c(0, 4), ylim = c(0, 6), type = "n", 
-		    xaxt = "n", yaxt = "n", xlab = "", ylab = "Model",
-                    #main = "Monoculture                                                                                                         Mixture")
-                    main = "Monoculture                                    Mixture                                       Monoculture                                    Mixture")
-               #+ theme_bw() + geom_line()
+df <- df %>%
+    pivot_longer(c(-SpeciesID, -Biomass, -type_bm, -Model, -log_bm), names_to= "var", values_to = "value")
+df <- merge(df,labels, by=c("Model","var"))
+print(head(df))
 
-#rasterImage(image, xleft, ybottom, xright, ytop, angle = 0, interpolate = TRUE, …)
+# Subset to plot loess line for all models, except Dryland
+df_noD <- df[df$Model != "Dryland",]
 
-rasterImage(G1C1, 0, 5, 1, 6)
-rasterImage(G1C2, 1, 5, 2, 6)
-rasterImage(G1C3, 2, 5, 3, 6)
-rasterImage(G1C4, 3, 5, 4, 6)
-rasterImage(G2C1, 0, 4, 1, 5)
-rasterImage(G2C2, 1, 4, 2, 5)
-rasterImage(G2C3, 2, 4, 3, 5)
-rasterImage(G2C4, 3, 4, 4, 5)
-rasterImage(G3C1, 0, 3, 1, 4)
-rasterImage(G3C2, 1, 3, 2, 4)
-rasterImage(G3C3, 2, 3, 3, 4)
-rasterImage(G3C4, 3, 3, 4, 4)
-rasterImage(F1C1, 0, 2, 1, 3)
-rasterImage(F1C2, 1, 2, 2, 3)
-rasterImage(F1C3, 2, 2, 3, 3)
-rasterImage(F1C4, 3, 2, 4, 3)
-rasterImage(F2C1, 0, 1, 1, 2)
-rasterImage(F2C2, 1, 1, 2, 2)
-rasterImage(F2C3, 2, 1, 3, 2)
-rasterImage(F2C4, 3, 1, 4, 2)
-rasterImage(DC1, 0, 0, 1, 1)
-rasterImage(DC2, 1, 0, 2, 1)
-rasterImage(DC3, 2, 0, 3, 1)
-rasterImage(DC4, 3, 0, 4, 1)
-while (!is.null(dev.list()))  dev.off()
-
-#path <- paste0(tmp_dir,"/traits_vs_biomass/")
-#png(paste0(path,"all_scatter_plots_productivity.png"), 
-#    width = 1663, height = 1530.5, units = "px", pointsize = 40,  res = NA,
-#    bg = "white", type = c("cairo", "cairo-png", "Xlib", "quartz"))
-#par(mar=c(0,0,1,0))
-#all_scatter <- plot(NA, xlim = c(0, 2), ylim = c(0, 3), type = "n", 
-#		    xaxt = "n", yaxt = "n", xlab = "", ylab = "",
-#                    main = "Monoculture                                   Mixture")
-#               #+ theme_bw() + geom_line()
-#
-##rasterImage(image, xleft, ybottom, xright, ytop, angle = 0, interpolate = TRUE, …)
-#
-#rasterImage(F1C1_P, 0, 2, 1, 3)
-#rasterImage(F1C3_P, 1, 2, 2, 3)
-#rasterImage(F2C1_P, 0, 1, 1, 2)
-#rasterImage(F2C3_P, 1, 1, 2, 2)
-#rasterImage(DC1_P, 0, 0, 1, 1)
-#rasterImage(DC3_P, 1, 0, 2, 1)
-#while (!is.null(dev.list()))  dev.off()
-#
+plot_name <- "Figure3.png"
+level1 <- c("Grass 1", "Forest 1", "Grass 2", "Grass 3", "Forest 2", "Dryland")
+level2 <- c("Monoculture","Mixture")
+p <- ggplot() +
+    geom_point(data = df, aes(x=value, y=log_bm)) +
+    #geom_smooth(method="lm", fill=NA) +
+    stat_smooth(data = df_noD, aes(x=value, y=log_bm), fullrange = TRUE, color="red", method="loess", se=FALSE) +
+    labs(y = "log biomass") +
+    #ggtitle(df$Model) + 
+    geom_text(data = df, aes(x=value, y=log_bm, label = Lab)) +
+    facet_grid2(factor(Model,levels=level1) ~ var + factor(type_bm,levels=level2), scales = "free", independent = "all", axes = "all") +
+    #facet_wrap(factor(Model,levels=level1) ~ factor(type_bm,levels=level2) + var, scales = "free", ncol = 4) +
+    theme_bw() +
+    theme_classic() +
+    theme(text = element_text(size = 25), strip.text = element_text(size=25),
+          legend.text = element_text(size=20), legend.title = element_text(size=20))
+    ggsave(file=paste0(store_dir, plot_name)
+           , width=18, height=20, dpi=300)
+    while (!is.null(dev.list()))  dev.off()
